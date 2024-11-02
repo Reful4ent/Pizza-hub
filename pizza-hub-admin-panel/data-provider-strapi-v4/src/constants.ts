fd46d9cd84ec786b5247495fac7651b4c31f2c21
Reful4ent <dima2323fr3@gmail.com>
2024-11-02 16:19:09 +0700

    fix view: fixed view of product Card in config edit, now 2 cols: settings and card

--- pizza-hub-admin-panel/data-provider-strapi-v4/src/constants.ts
+++ pizza-hub-admin-panel/data-provider-strapi-v4/src/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://192.168.0.12:1337/api";
+export const API_URL = "http://192.168.0.107:1337/api";
 export const TOKEN_KEY = "strapi-jwt-token";
--- pizza-hub-admin-panel/data-provider-strapi-v4/src/pages/config/edit.tsx
+++ pizza-hub-admin-panel/data-provider-strapi-v4/src/pages/config/edit.tsx
@@ -1,20 +1,18 @@
 
 import {
-    Button, Card,
+    Button, Col,
     ColorPicker,
-    ColorPickerProps,
     ConfigProvider,
     Form,
     FormProps,
-    GetProp,
-    Input,
+    Input, Row,
     Select,
     Spin
 } from "antd";
-import {IButtonType, ICategory, IConfig} from "../../interfaces";
-import {useApiUrl, useCustom, useOne, useSelect} from "@refinedev/core";
+import {IButtonType, IConfig} from "../../interfaces";
+import {useApiUrl, useCustom, useSelect} from "@refinedev/core";
 import axios from "axios";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 
 
 
@@ -107,154 +105,162 @@ export const ConfigEdit = () => {
                     <Input />
                 </Form.Item>
 
-                <Form.Item
-                    label="Настройка стандартной карточки товара"
-                    style={{fontWeight: "bold"}}
-                >
-                    <Form.Item
-                        label="Тип кнопки на стандартной карточке товара"
-                        name={["productButtonType",'id']}
-                        style={{fontWeight: "normal"}}
-                    >
-                        <Select options={buttonTypeOptions}/>
-                    </Form.Item>
-
-                    <Form.Item
-                        label="Цвет кнопки"
-                        name="colorStandardProductButton"
-                        style={{fontWeight: "normal"}}
-                    >
-                        <ColorPicker
-                            format="hex"
-                            onChange={(_,hex)=> setColorStandardProductButton(hex)}
-                        />
-                    </Form.Item>
-
-                    <Form.Item
-                        label="Цвет кнопки при наведении"
-                        name="colorHoverStandardProductButton"
-                        style={{fontWeight: "normal"}}
-                    >
-                        <ColorPicker
-                            format="hex"
-                            onChange={(_,hex)=> setColorHoverStandardProductButton(hex)}
-                        />
-                    </Form.Item>
-
-                    <Form.Item
-                        label="Цвет тени на карточке товара"
-                        name="colorBoxShadowStandardProductButton"
-                        style={{fontWeight: "normal"}}
-                    >
-                        <ColorPicker
-                            format="hex"
-                            onChange={(_,hex)=> setColorBoxShadowStandardProductButton(hex)}
-                        />
-                    </Form.Item>
-                </Form.Item>
-
-                <Form.Item style={{width: '100%', display: "flex", justifyContent: "center"}}>
-                    <div
-                        style={{
-                            boxShadow: `0px 0px 49px 9px ${colorBoxShadowStandardProductButton ?? configData.colorBoxShadowStandardProductButton}`,
-                            width: "322px",
-                            height: "552px",
-                            padding: "25px",
-                            border: "1px solid #00000026",
-                            borderRadius: "0.5rem",
-                            display: "flex",
-                            flexDirection: "column",
-                        }}
-                    >
-                        <div
-                            style={{
-                                width: "100%",
-                                height: "55%",
-                                display: "flex",
-                                alignItems: "center",
-                                justifyContent: "center",
-                            }}
-                        >
-                            <img
-                                style={{
-                                    width: "100%",
-                                    height: "168.75px",
-                                    alignSelf: "center",
-                                    marginBottom: "10px",
-                                }}
-                                src="https://storage.yandexcloud.net/pjproduction/images/catalog/thumbs/cart/63461818fed6eacd388278095682489d.webp"
-                                alt="Пицца"
-                            />
-                        </div>
-                        <div
-                            style={{
-                                width: "100%",
-                                height: "45%",
-                                display: "flex",
-                                flexDirection: "column",
-                            }}
-                        >
-                            <p
-                                className="line-clamp-1"
-                                style={{
-                                    marginBottom: "5px",
-                                    fontWeight: "500",
-                                    fontSize: "22px",
-                                }}
+                <Form.Item>
+                    <Row>
+                        <Col span={12}>
+                            <Form.Item
+                                label="Настройка стандартной карточки товара"
+                                style={{fontWeight: "bold"}}
                             >
-                                Nunc sodales pulvinar neque, ut placerat est laoreet nec.
-                                Etiam finibus porttitor varius. Duis placerat magna neque, eu maximus tortor molestie quis.
-                                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                                Fusce at tellus rhoncus, vehicula lacus rutrum, maximus orci.
-                            </p>
-
-                            <p
-                                className="line-clamp-4 text-[#616773]"
-                                style={{
-                                    marginBottom: "5px",
-                                }}
-                            >
-                                Nunc sodales pulvinar neque, ut placerat est laoreet nec.
-                                Etiam finibus porttitor varius. Duis placerat magna neque, eu maximus tortor molestie quis.
-                                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                                Fusce at tellus rhoncus, vehicula lacus rutrum, maximus orci.
-                            </p>
-
-                            <p
-                                style={{
-                                    marginBottom: "5px",
-                                    fontWeight: "600",
-                                    fontSize: "18px",
-                                }}
-                            >
-                                Цена: 180 ₽
-                            </p>
-                            <ConfigProvider
-                                theme={{
-                                    components: {
-                                        Button: {
-                                            defaultBg: colorStandardProductButton ?? configData.colorStandardProductButton,
-                                            defaultColor: '#fff',
-                                            defaultActiveColor: colorHoverStandardProductButton ?? configData.colorHoverStandardProductButton,
-                                            colorPrimary: colorStandardProductButton ?? configData.colorStandardProductButton,
-                                            colorPrimaryHover: colorHoverStandardProductButton ?? configData.colorHoverStandardProductButton,
-                                            colorPrimaryActive: colorHoverStandardProductButton ?? configData.colorHoverStandardProductButton,
-                                            textTextColor: colorStandardProductButton ?? configData.colorStandardProductButton,
-                                            textTextHoverColor: colorHoverStandardProductButton ?? configData.colorHoverStandardProductButton,
-                                        }
-                                    },
-                                }}
-                            >
-                                <Button
-                                    className="rounded-lg font-semibold"
-                                    style={{height: "25%"}}
-                                    type={buttonTypeOptions[buttonType?.id - 1]?.label ?? 'default'}
+                                <Form.Item
+                                    label="Тип кнопки на стандартной карточке товара"
+                                    name={["productButtonType",'id']}
+                                    style={{fontWeight: "normal"}}
                                 >
-                                    Тестовая кнопка
-                                </Button>
-                            </ConfigProvider>
-                        </div>
-                    </div>
+                                    <Select options={buttonTypeOptions}/>
+                                </Form.Item>
+
+                                <Form.Item
+                                    label="Цвет кнопки"
+                                    name="colorStandardProductButton"
+                                    style={{fontWeight: "normal"}}
+                                >
+                                    <ColorPicker
+                                        format="hex"
+                                        onChange={(_,hex)=> setColorStandardProductButton(hex)}
+                                    />
+                                </Form.Item>
+
+                                <Form.Item
+                                    label="Цвет кнопки при наведении"
+                                    name="colorHoverStandardProductButton"
+                                    style={{fontWeight: "normal"}}
+                                >
+                                    <ColorPicker
+                                        format="hex"
+                                        onChange={(_,hex)=> setColorHoverStandardProductButton(hex)}
+                                    />
+                                </Form.Item>
+
+                                <Form.Item
+                                    label="Цвет тени на карточке товара"
+                                    name="colorBoxShadowStandardProductButton"
+                                    style={{fontWeight: "normal"}}
+                                >
+                                    <ColorPicker
+                                        format="hex"
+                                        onChange={(_,hex)=> setColorBoxShadowStandardProductButton(hex)}
+                                    />
+                                </Form.Item>
+                            </Form.Item>
+                        </Col>
+                        <Col span={12}>
+                            <Form.Item style={{width: '100%', display: "flex", justifyContent: "center"}}>
+                                <div
+                                    style={{
+                                        boxShadow: `0px 0px 49px 9px ${colorBoxShadowStandardProductButton ?? configData.colorBoxShadowStandardProductButton}`,
+                                        width: "322px",
+                                        height: "552px",
+                                        padding: "25px",
+                                        border: "1px solid #00000026",
+                                        borderRadius: "0.5rem",
+                                        display: "flex",
+                                        flexDirection: "column",
+                                        marginTop: "60px"
+                                    }}
+                                >
+                                    <div
+                                        style={{
+                                            width: "100%",
+                                            height: "55%",
+                                            display: "flex",
+                                            alignItems: "center",
+                                            justifyContent: "center",
+                                        }}
+                                    >
+                                        <img
+                                            style={{
+                                                width: "100%",
+                                                height: "168.75px",
+                                                alignSelf: "center",
+                                                marginBottom: "10px",
+                                            }}
+                                            src="https://storage.yandexcloud.net/pjproduction/images/catalog/thumbs/cart/63461818fed6eacd388278095682489d.webp"
+                                            alt="Пицца"
+                                        />
+                                    </div>
+                                    <div
+                                        style={{
+                                            width: "100%",
+                                            height: "45%",
+                                            display: "flex",
+                                            flexDirection: "column",
+                                        }}
+                                    >
+                                        <p
+                                            className="line-clamp-1"
+                                            style={{
+                                                marginBottom: "5px",
+                                                fontWeight: "500",
+                                                fontSize: "22px",
+                                            }}
+                                        >
+                                            Nunc sodales pulvinar neque, ut placerat est laoreet nec.
+                                            Etiam finibus porttitor varius. Duis placerat magna neque, eu maximus tortor molestie quis.
+                                            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                                            Fusce at tellus rhoncus, vehicula lacus rutrum, maximus orci.
+                                        </p>
+
+                                        <p
+                                            className="line-clamp-4 text-[#616773]"
+                                            style={{
+                                                marginBottom: "5px",
+                                            }}
+                                        >
+                                            Nunc sodales pulvinar neque, ut placerat est laoreet nec.
+                                            Etiam finibus porttitor varius. Duis placerat magna neque, eu maximus tortor molestie quis.
+                                            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                                            Fusce at tellus rhoncus, vehicula lacus rutrum, maximus orci.
+                                        </p>
+
+                                        <p
+                                            style={{
+                                                marginBottom: "5px",
+                                                fontWeight: "600",
+                                                fontSize: "18px",
+                                            }}
+                                        >
+                                            Цена: 180 ₽
+                                        </p>
+                                        <ConfigProvider
+                                            theme={{
+                                                components: {
+                                                    Button: {
+                                                        defaultBg: colorStandardProductButton ?? configData.colorStandardProductButton,
+                                                        defaultColor: '#fff',
+                                                        defaultActiveColor: colorHoverStandardProductButton ?? configData.colorHoverStandardProductButton,
+                                                        colorPrimary: colorStandardProductButton ?? configData.colorStandardProductButton,
+                                                        colorPrimaryHover: colorHoverStandardProductButton ?? configData.colorHoverStandardProductButton,
+                                                        colorPrimaryActive: colorHoverStandardProductButton ?? configData.colorHoverStandardProductButton,
+                                                        textTextColor: colorStandardProductButton ?? configData.colorStandardProductButton,
+                                                        textTextHoverColor: colorHoverStandardProductButton ?? configData.colorHoverStandardProductButton,
+                                                    }
+                                                },
+                                            }}
+                                        >
+                                            <Button
+                                                className="rounded-lg font-semibold"
+                                                style={{height: "25%"}}
+                                                type={buttonTypeOptions[buttonType?.id - 1]?.label ?? 'default'}
+                                            >
+                                                Тестовая кнопка
+                                            </Button>
+                                        </ConfigProvider>
+                                    </div>
+                                </div>
+                            </Form.Item>
+                        </Col>
+                    </Row>
                 </Form.Item>
 
                 <Form.Item>
@@ -262,5 +268,5 @@ export const ConfigEdit = () => {
                 </Form.Item>
             </Form>
         </>
-)
+    )
 }
